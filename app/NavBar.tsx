f3bd84242083b424ef0a3b5f50ccd97d16fe77f8
mohanraj-devalpha <mohanraj.s1039@ptuniv.edu.in>
2025-04-21 17:08:48 +0530

    navbar configurated

---
+++ app/NavBar.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import { AiFillBug } from "react-icons/ai";
+
+
+const NavBar = () => {
+  const links =[
+    {label: "Dashboard", href: "/"},
+    {label: "Issues", href: "/issues"},
+  ]
+  return (
+    <div className=''>
+    <nav className='flex items-center space-x-4 border-b border-green-300 py-5 px-5 mb-5'>
+      <Link href="/"><AiFillBug/></Link>
+      <ul className='list-none flex space-x-4'>
+        {links.map(link => <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+        
+      </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default NavBar
